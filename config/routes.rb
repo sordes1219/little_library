1cb9b4e1a26ea9fe6674f1f5529cdec8ffddde60
romak <romak@exaple.com>
2020-08-14 06:03:09 +0000

    Add rent/putback function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "application#hello"
+  
+  get '/login' => 'users#login_form'
+  post '/login' => 'users#login'
+  post '/logout' => 'users#logout'
+  
+  get '/books/rent' => 'books#index_rent'
+  get '/books/putback' => 'books#index_putback'
+  
+  get '/books/:book_id' => 'books#show'
+  post '/books/:book_id/rent' => 'books#rent'
+  post '/books/:book_id/putback' => 'books#putback'
+  
+  get 'users/:user_id/rent' => 'users#index_rent'
+  get 'users/:user_id/putback' => 'users#index_putback'
+  
+  
+  get '' => "home#top"
+  
 end
