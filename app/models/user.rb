08989d77c7e46c3d12205c842780b9299de6545c
EC2 Default User <ec2-user@ip-172-31-5-199.ec2.internal>
2020-08-24 14:58:00 +0000

    Implement security features

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
   validates :name,{presence: true}
   validates :group,{presence: true}
   validates :email,{presence: true,uniqueness: true}
+  validates :password,{presence: true, length: {minimum: 8}}
   
 end
